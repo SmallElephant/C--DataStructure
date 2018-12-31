31a33add53acc48f73759cf845e5483fc0de0ac3
FlyElephant <test@qq.com>
2018-12-31 12:09:50 +0800

    查找指定二维数组中数字是否存在

--- 3-array/3-array/main.cpp
+++ 3-array/3-array/main.cpp
@@ -8,9 +8,8 @@
 
 #include <iostream>
 
-int main() {
-    int len = 7;
-    int arr[7] = {2,3,1,0,2,5,3};
+// 查找数组中重复的数字
+void findRepeatedNumber(int *arr,int len) {
     int map[len];
     for (int i = 0; i < len; i++) {
         map[i] = 0;
@@ -23,6 +22,37 @@ int main() {
         if (map[i] > 1) {
             printf("重复的数字:%d---重复次数:%d\n",i,map[i]);
         }
+    }
+}
+
+// 二维数组中查找指定的数字是否存在
+
+bool findTargetNumber(int arr[][4],int num,int cols,int rows) {
+    int row = 0;
+    int col = cols - 1;
+    while (row < cols && col >= 0) {
+        int target = arr[row][col];
+        if (num == arr[row][col]) {
+            return true;
+        } else if (num < target) {
+            col--;
+        } else {
+            row++;
+        }
+    }
+    return false;
+}
+
+int main() {
+    int arr[7] = {2,3,4,5,2,3,1};
+    findRepeatedNumber(arr, 7);
+    int data[4][4] = {{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
+    int num = 100;
+    bool res = findTargetNumber(data, num, 4, 4);
+    if (res) {
+        printf("%d exist\n",10);
+    } else {
+        printf("not exist\n");
     }
     return 0;
 }
