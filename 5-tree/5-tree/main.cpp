b705a1f287ad51da59f4239540b3992888f48395
FlyElephant <test@qq.com>
2019-01-01 00:15:26 +0800

    打印二叉树的所有路径

--- 5-tree/5-tree/main.cpp
+++ 5-tree/5-tree/main.cpp
@@ -107,16 +107,54 @@ void postorderNonrecursion(BTNode *root) {
     printf("\n");
 }
 
+void printPath(char path[],int len) {
+    for (int i = 0; i < len; i++) {
+        printf("%c->",path[i]);
+    }
+    printf("\n");
+}
+
+void printTargetPath(char path[], int len,int target) {
+    int sum = 0;
+    char *res = (char *)malloc(sizeof(char) * len);
+    for (int i = 0; i < len; i++) {
+        sum += path[i] - '0';
+        res[i] = path[i];
+    }
+    if (sum == target) {
+        printf("%s\n",res);
+    }
+}
+
+void printAllPaths(BTNode *root,char paths[],int len) {
+    if (root == NULL) {
+        return;
+    }
+    paths[len++] = root->data;
+    if (root->lchild == NULL && root->rchild == NULL) {
+        printPath(paths, len);
+//        printTargetPath(paths, len, 15);
+    }
+    printAllPaths(root->lchild, paths, len);
+    printAllPaths(root->rchild, paths, len);
+}
+
 int main() {
     BTNode node1 = {'1',NULL,NULL};
     BTNode node2 = {'2',NULL,NULL};
     BTNode node3 = {'3',NULL,NULL};
     BTNode node4 = {'4',NULL,NULL};
     BTNode node5 = {'5',NULL,NULL};
+    BTNode node6 = {'6',NULL,NULL};
+    BTNode node7 = {'7',NULL,NULL};
+    BTNode node8 = {'8',NULL,NULL};
     node1.lchild = &node2;
     node1.rchild = &node4;
     node2.lchild = &node3;
     node2.rchild = &node5;
+    node4.lchild = &node6;
+    node5.lchild = &node7;
+    node5.rchild = &node8;
     preOrder(&node1);
     printf("\n");
     preorderNorecursion(&node1);
@@ -129,5 +167,7 @@ int main() {
     printf("\n");
     printf("后序非递归遍历:");
     postorderNonrecursion(&node1);
+    char paths[maxsize];
+    printAllPaths(&node1, paths, 0);
     return 0;
 }
