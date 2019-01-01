9f23f02826096202b13330851405ac1a0e0bce62
FlyElephant <test@qq.com>
2019-01-01 17:41:03 +0800

    二叉树的构建

--- 5-tree/5-tree/main.cpp
+++ 5-tree/5-tree/main.cpp
@@ -185,6 +185,8 @@ void levelorder(BTNode *root) {
     printf("\n");
 }
 
+// 二叉树宽度
+
 int maxTreeWidth(BTNode *root) {
     if (root==NULL) {
         return 0;
@@ -228,7 +230,42 @@ int maxTreeWidth(BTNode *root) {
     return res;
 }
 
-// 二叉树的宽度
+// 二叉树的重建
+
+int findValue(char arr[],char val,int s,int e) {
+    int index = -1;
+    for (int i = s; i <= e; i++) {
+        if (arr[i] == val) {
+            index = i;
+        }
+    }
+    return index;
+}
+
+BTNode *consturct(char preorder[],int prestart,int preend,char inorder[],int instart,int inend) {
+    if (prestart > preend || instart > inend) {
+        return NULL;
+    }
+    BTNode *root=(BTNode *)malloc(sizeof(BTNode));
+    if (prestart == preend && instart==inend) {
+        root->data = preorder[prestart];
+        root->lchild = NULL;
+        root->rchild = NULL;
+        return root;
+    }
+    char first = preorder[prestart];
+    int index = findValue(inorder, first, instart, inend);
+    int leftLen = index - instart; //左子树的长度
+    int rightLen = inend - index; //右子树长度
+    int start1 = prestart+1;
+    int end1 = prestart+leftLen;
+    int start2 = instart;
+    int end2 = instart+leftLen-1;
+    root->data = first;
+    root->lchild = consturct(preorder, start1, end1, inorder, start2, end2);
+    root->rchild = consturct(preorder, end1+1, end1+rightLen, inorder, end2+2, end2+rightLen+1);
+    return root;
+}
 
 int main() {
     BTNode node1 = {'1',NULL,NULL};
@@ -246,10 +283,12 @@ int main() {
     node4.lchild = &node6;
     node5.lchild = &node7;
     node5.rchild = &node8;
+    printf("先序遍历：\n");
     preOrder(&node1);
     printf("\n");
     preorderNorecursion(&node1);
     printf("\n");
+    printf("中序遍历:\n");
     inorder(&node1);
     printf("\n");
     inorderNonrecursion(&node1);
@@ -264,5 +303,11 @@ int main() {
     printf("binary tree height:%d\n",height);
     levelorder(&node1);
     maxTreeWidth(&node1);
+    char preorder1[] = "12357846";
+    char inorder1[] = "32758164";
+    BTNode *rootTest = consturct(preorder1, 0, 7, inorder1, 0, 7);
+    printf("根结点的值:%c\n",rootTest->data);
+    inorder(rootTest);
+    printf("\n");
     return 0;
 }
