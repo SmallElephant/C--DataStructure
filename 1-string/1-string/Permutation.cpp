2fbcd6361ba0591e154b1f604e52460ff6793ffa
FlyElephant <test@qq.com>
2019-01-07 17:21:50 +0800

    字符串的全排列算法

--- 1-string/1-string/Permutation.cpp
+++ 1-string/1-string/Permutation.cpp
@@ -7,25 +7,37 @@
 //
 
 #include <iostream>
+#include <string>
+using namespace std;
 
 class Permute {
 public:
+    // 全排列就是字符串的每个字符分别和其他的字符进行交换
     void permute(char *str,int l,int r) {
         if (l==r) {
             printf("%s\n",str);
+            return;
         }
-        for (int i=l; i <= r ; i++) {
-            swap((str+l), (str+i));
+        for (int i = l; i <= r; i++) {
+            swap((str+l), (str+i));// 首个字符和其他的字符进行交换
             permute(str, l+1, r);
-            swap((str+l), (str+i));
+            swap((str+l), (str+i));//恢复原状
         }
     }
     
     void test() {
         char p[] = "abc";
+        printf("字符串的全排列:\n");
         permute(p, 0, 2);
-    }
+        printf("字符串的所有子串:\n");    }
 private:
+    void printChar(char *str,int l,int r) {
+        for (int i = l; i <= r; i++) {
+            printf("%c",*(str+i));
+        }
+        printf("\n");
+    }
+    
     void swap(char *a,char *b) {
         int tmp = *a;
         *a = *b;
