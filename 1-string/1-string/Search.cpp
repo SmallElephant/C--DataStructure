a5dd8b4e6422b7e11a7f430246d01d6a7afc69a0
FlyElephant <test@qq.com>
2019-01-11 23:56:18 +0800

    第一个只出现一次字符的位置

--- 1-string/1-string/Search.cpp
+++ 1-string/1-string/Search.cpp
@@ -7,6 +7,7 @@
 //
 
 #include <iostream>
+#include <map>
 using namespace std;
 
 class Search {
@@ -17,9 +18,27 @@ public:
         return val - '0';
     }
     
+    int findNotRepeatingNumber(string str) {
+        map<char,int> hashMap;
+        for (int i = 0; i < str.size(); i++) {
+            hashMap[str[i]] += 1;
+        }
+        int index = -1;
+        for (int i = 0; i < str.size(); i++) {
+            if (hashMap[str[i]] == 1) {
+                index = i;
+                break;
+            }
+        }
+        return index;
+    }
+    
     void test() {
         char str[] = "012345678910";
         int val = findNumber(str, 5);
         printf("字符串中的数字:%d\n",val);
+        string repeatingStr = "abaccdeff";
+        int index = findNotRepeatingNumber(repeatingStr);
+        printf("第一个只出现一次字符的位置:%d\n",index);
     }
 };
