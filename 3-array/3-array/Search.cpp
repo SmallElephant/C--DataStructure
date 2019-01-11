43dcb04ac38a8b3e8d47a7ef952be67159c545e3
FlyElephant <test@qq.com>
2019-01-11 16:14:02 +0800

    1到N中数字1出现的次数

--- 3-array/3-array/Search.cpp
+++ 3-array/3-array/Search.cpp
@@ -12,6 +12,41 @@ using namespace std;
 class Search {
 public:
     
+    // 查找数组中重复的数字
+    void findRepeatedNumber(int *arr,int len) {
+        int map[len];
+        for (int i = 0; i < len; i++) {
+            map[i] = 0;
+        }
+        for (int i = 0; i < len; i++) {
+            int index = arr[i] % len;
+            map[index] = map[index] + 1;
+        }
+        for (int i = 0; i < len; i++) {
+            if (map[i] > 1) {
+                printf("重复的数字:%d---重复次数:%d\n",i,map[i]);
+            }
+        }
+    }
+    
+    // 二维数组中查找指定的数字是否存在
+    
+    bool findTargetNumber(int arr[][4],int num,int cols,int rows) {
+        int row = 0;
+        int col = cols - 1;
+        while (row < cols && col >= 0) {
+            int target = arr[row][col];
+            if (num == arr[row][col]) {
+                return true;
+            } else if (num < target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+    
     // 数组中出现超过一半的数字
     int findMoreHalfNumber(int arr[],int n) {
         if (n <= 0) {
@@ -56,17 +91,49 @@ public:
         return -1;
     }
     
-    void test() {
-        int arr[] = {1,2,3,4,5,6,1,1,1};
-        int val = findMoreHalfNumber(arr, 9);
-        if (val > 0) {
-            printf("超过一半的数字:%d\n",val);
-        } else {
-            printf("没有超过一半的数字\n");
+    int numberOf1(int num) {
+        int count = 0;
+        while (num) {
+            if (num % 10 == 1) {
+                count++;
+            }
+            num = num / 10;
         }
-        int search[] = {3,8,9,10,11,12};
-        int index =binarySearch(search, 8, 6);
-        printf("二分查找位置:%d\n",index);
+        return count;
+    }
+    
+    // 1~n整数中1出现的次数
+    int numOf1From1ToN(int n) {
+        int count = 0;
+        for (int i = 0; i <= n; i++) {
+            count += numberOf1(i);
+        }
+        return count;
+    }
+    
+    void test() {
+//        int arr[7] = {2,3,4,5,2,3,1};
+//        findRepeatedNumber(arr, 7);
+//        int data[4][4] = {{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
+//        int num = 100;
+//        bool res = findTargetNumber(data, num, 4, 4);
+//        if (res) {
+//            printf("%d exist\n",10);
+//        } else {
+//            printf("not exist\n");
+//        }
+//        int arr1[] = {1,2,3,4,5,6,1,1,1};
+//        int val = findMoreHalfNumber(arr1, 9);
+//        if (val > 0) {
+//            printf("超过一半的数字:%d\n",val);
+//        } else {
+//            printf("没有超过一半的数字\n");
+//        }
+//        int search[] = {3,8,9,10,11,12};
+//        int index =binarySearch(search, 8, 6);
+//        printf("二分查找位置:%d\n",index);
+        int count = numOf1From1ToN(12);
+        printf("1出现的总共次数:%d\n",count);
     }
 };
 
--- 3-array/3-array/main.cpp
+++ 3-array/3-array/main.cpp
@@ -18,40 +18,6 @@ void printList(int *arr, int len) {
     printf("\n");
 }
 
-// 查找数组中重复的数字
-void findRepeatedNumber(int *arr,int len) {
-    int map[len];
-    for (int i = 0; i < len; i++) {
-        map[i] = 0;
-    }
-    for (int i = 0; i < len; i++) {
-        int index = arr[i] % len;
-        map[index] = map[index] + 1;
-    }
-    for (int i = 0; i < len; i++) {
-        if (map[i] > 1) {
-            printf("重复的数字:%d---重复次数:%d\n",i,map[i]);
-        }
-    }
-}
-
-// 二维数组中查找指定的数字是否存在
-
-bool findTargetNumber(int arr[][4],int num,int cols,int rows) {
-    int row = 0;
-    int col = cols - 1;
-    while (row < cols && col >= 0) {
-        int target = arr[row][col];
-        if (num == arr[row][col]) {
-            return true;
-        } else if (num < target) {
-            col--;
-        } else {
-            row++;
-        }
-    }
-    return false;
-}
 
 // 青蛙跳级与斐波那契数列
 int fibonacci(int n) {
@@ -171,16 +137,6 @@ void tesClockArray() {
 }
 
 int main() {
-    int arr[7] = {2,3,4,5,2,3,1};
-    findRepeatedNumber(arr, 7);
-    int data[4][4] = {{1,2,8,9},{2,4,9,12},{4,7,10,13},{6,8,11,15}};
-    int num = 100;
-    bool res = findTargetNumber(data, num, 4, 4);
-    if (res) {
-        printf("%d exist\n",10);
-    } else {
-        printf("not exist\n");
-    }
     int num1 = 5;
     int count = fibonacci(num1);
     printf("%d级台阶的总共步数:%d\n",num1,count);
@@ -189,11 +145,11 @@ int main() {
     printList(order, 9);
     tesClockArray();
     printf("\n");
-    Search searchData;
-    searchData.test();
     Sort sort;
     sort.test();
     Sum sum;
     sum.test();
+    Search searchData;
+    searchData.test();
     return 0;
 }
